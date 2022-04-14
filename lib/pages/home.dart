185052305778892f26de0a8b16a071da2b8e2502
Ritik Rajoria <ritikrajrrr@gmail.com>
2022-04-14 22:02:10 +0530

    home page completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -42,7 +42,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     isLoading = true;
-    // getData();
 
     photoDBHelper = PhotoDBHelper();
     favdbHelper = FavDBHelper();
